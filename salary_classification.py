c87e4e6d8a8d25658d5bf3651c2933408c255cc4
Prashar4801 <Prashar4801@gmail.com>
2022-10-29 14:59:58 +0100

    6006CEM: Added functions to print data and remove spaces in df
    The following commit:
    - adds a function to check all data
    - adds a function to remove spaces in the dataframe

--- salary_classification.py
+++ salary_classification.py
@@ -5,6 +5,14 @@ import numpy as np
 import matplotlib as plt
 import seaborn as sns
 
+def check_data(df):
+    print(df.shape)
+    print(df.info())
+    print(df.describe())
+    print(df.nunique())
+    print(df.isnull().sum())
+    print(df.duplicated().sum())
+
 """
     The following function aims to preprocess the data so that is in the correct format for training:
     This includes:
@@ -26,8 +34,8 @@ def train_data():
 def remove_null_values(df):
     #set  ? to be null values
     df.replace(" ?", pd.NA, inplace=True)
-    null_values = df.isnull()
-    if (null_values.sum().sum()) > 0:
+    null_values = df.isnull().sum().sum()
+    if (null_values) > 0:
         print("Null Values are present... removing relevant data samples")
         df.dropna(inplace=True)
     return df
@@ -39,10 +47,18 @@ def remove_duplicates(df):
         df.drop_duplicates(keep='first',inplace=True)
     return df
 
+def remove_spaces(df):
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            df[col] = df[col].map(str.strip)
+
 def main():
     df = pd.read_csv("salary.csv")
+    # check_data(df)
+    remove_spaces(df)
     remove_null_values(df)
     remove_duplicates(df)
+
 
 if __name__ == "__main__":
     main()
