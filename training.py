5f90aa6dfd81afb42136d16c74c8f13c10c079d1
Prashar4801 <Prashar4801@gmail.com>
2022-11-10 12:04:47 +0000

    6006CEM: Added training using the knearest algorithm
    The following commit:
    - Updates training file to assign X_test,X_train,Y_test_y_train
    - Adds function to run nearest neighbour algorithm

--- training.py
+++ training.py
@@ -2,5 +2,39 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 import seaborn as sns
+import sklearn
 
 import preprocessing
+
+
+def train_model(df):
+    
+    """
+    Converts all categorical data to numerical
+    """
+    
+    X = df.iloc[:, :-1]
+    y = df.iloc[:, -1]
+
+    from sklearn.model_selection import train_test_split
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=0
+    )
+
+    k_nearest_algorithm(X_train, y_train, X_test, y_test)
+
+
+def k_nearest_algorithm(X_train, y_train, X_test, y_test):
+    """
+    Converts all categorical data to numerical
+    """
+    from sklearn.neighbors import KNeighborsClassifier
+
+    knn = KNeighborsClassifier(n_neighbors=20)
+    knn.fit(X_train, y_train)
+    print("accuracy:")
+    print(knn.score(X_test, y_test))
+    return knn
+
+
